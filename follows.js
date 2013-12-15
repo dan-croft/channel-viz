10d098380a0b8f47fb3b35ebe62317903749d0c0
dan-croft <croft.barton@gmail.com>
2013-12-14 20:13:09 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,16 +1,16 @@
 (function ( $ ){
 
-	/*
+
 	EXAMPLE CONFIGURATION
 
-		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
+		var defaultKey	= 'ZEkBW5fj0t1zUwTjky4twTBSfIwc3qJOJW6SWxE5e2ZiKHk2', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1875709359], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'your mom', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0;
-	*/
+
 
 	var defaultKey		= '', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
