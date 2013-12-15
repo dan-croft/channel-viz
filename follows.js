d03d3eab2834dab0dcc923c12cf4a30140c9508b
dan-croft <croft.barton@gmail.com>
2013-12-14 20:17:04 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -4,7 +4,7 @@
 		var defaultKey	= 'ZEkBW5fj0t1zUwTjky4twTBSfIwc3qJOJW6SWxE5e2ZiKHk2', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1875709359], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'your mom', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '1hour', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1;
